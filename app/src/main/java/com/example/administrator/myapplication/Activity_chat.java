2bfbf3f2b838858ddf3a265e87590b37948aa5f6
Julien Grave <darkin@live.fr>
2015-07-27 13:12:10 +0200

    First commit

---
+++ app/src/main/java/com/example/administrator/myapplication/Activity_chat.java
@@ -0,0 +1,18 @@
+package com.example.administrator.myapplication;
+
+
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.widget.Button;
+import android.view.View;
+
+public class Activity_chat extends ActionBarActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.chat);
+    }
+}
